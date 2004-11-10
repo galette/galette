c8241824b8868d0b85215811aeb06e06a2f2db26
Laurent Pelecq <laurent.pelecq@soleil.org>
2004-11-10 23:45:29 +0000

    Fixed upgrade of galette_preferences for psql. Added upgrade of galette_cotisations.

--- galette/install/sql/upgrade-to-0.63-pgsql.sql
+++ galette/install/sql/upgrade-to-0.63-pgsql.sql
@@ -8,14 +8,18 @@ CREATE UNIQUE INDEX galette_statuts_idx ON galette_statuts (id_statut);
 CREATE UNIQUE INDEX galette_types_cotisation_idx ON galette_types_cotisation (id_type_cotis);
 CREATE UNIQUE INDEX galette_logs_idx ON galette_logs (id_log);
 
-DELETE FROM galette_preferences WHERE nom_pref == 'pref_pays';
+-- Fix table preference with duplicate ids and create index;
+UPDATE galette_preferences SET id_pref=id_pref+1 WHERE (id_pref >= 4 AND nom_pref != 'pref_ville');
+UPDATE galette_preferences SET id_pref=id_pref+1 WHERE (id_pref >= 2 AND nom_pref != 'pref_adresse');
 CREATE UNIQUE INDEX galette_preferences_idx ON galette_preferences (id_pref);
-INSERT INTO galette_preferences (nom_pref, val_pref) VALUES ('pref_pays', '');
-INSERT INTO galette_preferences (nom_pref, val_pref) VALUES ('pref_mail_method', '0');
-INSERT INTO galette_preferences (nom_pref, val_pref) VALUES ('pref_mail_smtp', '0');
-INSERT INTO galette_preferences (nom_pref, val_pref) VALUES ('pref_membership_ext', '12');
-INSERT INTO galette_preferences (nom_pref, val_pref) VALUES ('pref_beg_membership', '');
+-- Add new or missing preferences
+INSERT INTO galette_preferences VALUES (22, 'pref_mail_method', '0');
+INSERT INTO galette_preferences VALUES (23, 'pref_mail_smtp', '0');
+INSERT INTO galette_preferences VALUES (24, 'pref_membership_ext', '12');
+INSERT INTO galette_preferences VALUES (25, 'pref_beg_membership', '');
 
+-- New tables for dynamic fields;
+DROP SEQUENCE galette_categories_id_seq;
 CREATE SEQUENCE galette_categories_id_seq
     START 1
     INCREMENT 1
@@ -23,6 +27,7 @@ CREATE SEQUENCE galette_categories_id_seq
     MINVALUE 1
     CACHE 1;
  
+DROP TABLE galette_info_categories;
 CREATE TABLE galette_info_categories (
   id_cat integer DEFAULT nextval('galette_categories_id_seq'::text) NOT NULL,
   index_cat integer DEFAULT '0' NOT NULL,
@@ -33,7 +38,8 @@ CREATE TABLE galette_info_categories (
   contents_cat text DEFAULT ''
 );
 CREATE UNIQUE INDEX galette_info_categories_idx ON galette_info_categories (id_cat);
- 
+
+DROP TABLE galette_adh_info; 
 CREATE TABLE galette_adh_info (
   id_adh integer DEFAULT '0' NOT NULL,
   id_cat integer DEFAULT '0' NOT NULL,
@@ -41,3 +47,20 @@ CREATE TABLE galette_adh_info (
   val_info text DEFAULT ''
 );
 CREATE INDEX galette_ahd_info_idx ON galette_adh_info (id_adh);
+
+-- Change table cotisations to store date_fin_cotis instead of duration;
+ALTER TABLE galette_cotisations ADD date_enreg date;
+ALTER TABLE galette_cotisations ADD date_debut_cotis date;
+ALTER TABLE galette_cotisations ADD date_fin_cotis date;
+UPDATE galette_cotisations
+	SET date_enreg=date_cotis,
+	    date_debut_cotis=date_cotis,
+	    date_fin_cotis=date_cotis +  to_char(duree_mois_cotis, '99" month"')::interval;
+ALTER TABLE galette_cotisations ALTER COLUMN date_enreg SET NOT NULL;
+ALTER TABLE galette_cotisations ALTER COLUMN date_enreg SET DEFAULT '00000101';
+ALTER TABLE galette_cotisations ALTER COLUMN date_debut_cotis SET NOT NULL;
+ALTER TABLE galette_cotisations ALTER COLUMN date_debut_cotis SET DEFAULT '00000101';
+ALTER TABLE galette_cotisations ALTER COLUMN date_fin_cotis SET NOT NULL;
+ALTER TABLE galette_cotisations ALTER COLUMN date_fin_cotis SET DEFAULT '00000101';
+ALTER TABLE galette_cotisations DROP duree_mois_cotis;
+ALTER TABLE galette_cotisations DROP date_cotis;
