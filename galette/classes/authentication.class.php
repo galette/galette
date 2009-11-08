446221e6131ccb2648c774527f0031437a22440a
Johan Cwiklinski <johan@x-tnd.be>
2009-11-08 21:04:21 +0000

    Applying PEAR Coding Standards (http://pear.php.net/manual/en/standards.php)

--- galette/classes/authentication.class.php
+++ galette/classes/authentication.class.php
@@ -1,117 +1,199 @@
 <?php
 
-// Copyright © 2009 Johan Cwiklinski
-//
-// This file is part of Galette (http://galette.tuxfamily.org).
-//
-// Galette is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Galette is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Galette. If not, see <http://www.gnu.org/licenses/>.
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * authentication.class.php, 28 février 2009
+ * Abstract authentication class for galette
  *
- * @package Galette
- * 
- * @author     Johan Cwiklinski <johan@x-tnd.be>
- * @copyright  2009 Johan Cwiklinski
- * @license    http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
- * @version    $Id$
- * @since      Disponible depuis la Release 0.7alpha
+ * PHP version 5
+ *
+ * Copyright © 2009 The Galette Team
+ *
+ * This file is part of Galette (http://galette.tuxfamily.org).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Authentication
+ * @package   Galette
+ *
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2009 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @version   SVN: $Id$
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.7dev - 2009-02-28
  */
 
 /**
  * Abstract authentication class for galette
  *
- * @name Authentication
- * @package Galette
- *
+ * @category  Classes
+ * @name      Authentication
+ * @package   Galette
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2009 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.7dev - 2009-02-28
  */
 
-abstract class Authentication{
-	private $login;
-	private $passe;
-	private $name;
-	private $surname;
-	private $admin = false;
-	private $id;
-	private $lang;
-	private $logged = false;
-	private $active = false;
-	private $superadmin = false;
+abstract class Authentication
+{
+    private $_login;
+    private $_passe;
+    private $_name;
+    private $_surname;
+    private $_admin = false;
+    private $_id;
+    private $_lang;
+    private $_logged = false;
+    private $_active = false;
+    private $_superadmin = false;
 
-	/**
-	* Default constructor
-	*/
-	public function __construct(){}
+    /**
+    * Default constructor
+    */
+    public function __construct()
+    {
+    }
 
-	/**
-	* Logs in user.
-	* @param user user's login
-	* @param passe md5 hashed password
-	* @returns integer state : 
-	* 	'-1' if there were an error
-	*	'-10' if user cannot login (mistake or user doesn't exists)
-	*	'1' if user were logged in successfully
-	*/
-	abstract public function logIn($user, $passe);
+    /**
+    * Logs in user.
+    *
+    * @param string $user  user's login
+    * @param string $passe md5 hashed password
+    *
+    * @return integer state :
+    *     '-1' if there were an error
+    *    '-10' if user cannot login (mistake or user doesn't exists)
+    *    '1' if user were logged in successfully
+    */
+    abstract public function logIn($user, $passe);
 
-	/**
-	* Does this login already exists ?
-	* These function should be used for setting admin login into Preferences
-	* @param user the username
-	* @return true if the username already exists, false otherwise
-	*/
-	abstract public function loginExists($user);
+    /**
+    * Does this login already exists ?
+    * These function should be used for setting admin login into Preferences
+    *
+    * @param string $user the username
+    *
+    * @return true if the username already exists, false otherwise
+    */
+    abstract public function loginExists($user);
 
-	/**
-	* Login for the superuser
-	* @param login login name
-	*/
-	public function logAdmin($login){
-		$this->logged = true;
-		$this->name = 'Admin';
-		$this->login = $login;
-		$this->admin = true;
-		$this->active = true;
-		//a flag for super admin only, since it's not a regular user
-		$this->superadmin = true;
-	}
+    /**
+    * Login for the superuser
+    *
+    * @param string $login name
+    *
+    * @return void
+    */
+    public function logAdmin($login)
+    {
+        $this->_logged = true;
+        $this->_name = 'Admin';
+        $this->_login = $login;
+        $this->_admin = true;
+        $this->_active = true;
+        //a flag for super admin only, since it's not a regular user
+        $this->_superadmin = true;
+    }
 
-	/**
-	* Log out user and unset variables
-	*/
-	public function logOut(){
-		$this->logged = false;
-		$this->name = null;
-		$this->login = null;
-		$this->admin = false;
-		$this->active = false;
-	}
+    /**
+    * Log out user and unset variables
+    *
+    * @return void
+    */
+    public function logOut()
+    {
+        $this->_logged = false;
+        $this->_name = null;
+        $this->_login = null;
+        $this->_admin = false;
+        $this->_active = false;
+        $this->_superadmin = false;
+    }
 
-	/* GETTERS */
-	public function isLogged(){return $this->logged;}
-	public function isAdmin(){return $this->admin;}
-	public function isSuperAdmin(){return $this->superadmin;}
-	public function isActive(){return $this->active;}
-	public function __get($name){
-		$forbidden = array('logged', 'admin', 'active');
-		if( !in_array($name, $forbidden) )
-			return $this->$name;
-		else return false;
-	}
-	/* SETTERS */
-	public function __set($name, $value) {
-		$this->$name = $value;
-	}
+    /**
+    * Is user logged-in?
+    *
+    * @return bool
+    */
+    public function isLogged()
+    {
+        return $this->_logged;
+    }
+
+    /**
+    * Is user admin?
+    *
+    * @return bool
+    */
+    public function isAdmin()
+    {
+        return $this->_admin;
+    }
+
+    /**
+    * Is user super admin?
+    *
+    * @return bool
+    */
+    public function isSuperAdmin()
+    {
+        return $this->_superadmin;
+    }
+
+    /**
+    * Is user active?
+    *
+    * @return bool
+    */
+    public function isActive()
+    {
+        return $this->_active;
+    }
+
+    /**
+    * Global getter method
+    *
+    * @param string $name name of the property we want to retrive
+    *
+    * @return false|object the called property
+    */
+    public function __get($name)
+    {
+        $forbidden = array('logged', 'admin', 'active');
+        if ( !in_array($name, $forbidden) ) {
+            $name = '_' . $name;
+            return $this->$name;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+    * Global setter method
+    *
+    * @param string $name  name of the property we want to assign a value to
+    * @param object $value a relevant value for the property
+    *
+    * @return void
+    */
+    public function __set($name, $value)
+    {
+        $name = '_' . $name;
+        $this->$name = $value;
+    }
 }
 ?>
--- galette/classes/galette-login.class.php
+++ galette/classes/galette-login.class.php
@@ -1,113 +1,143 @@
 <?php
 
-// Copyright © 2007-2009 Johan Cwiklinski
-//
-// This file is part of Galette (http://galette.tuxfamily.org).
-//
-// Galette is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Galette is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Galette. If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * galette-login.class.php, 06 juillet 2007
- *
- * @package Galette
- * 
- * @author     Johan Cwiklinski <johan@x-tnd.be>
- * @copyright  2007-2009 Johan Cwiklinski
- * @license    http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
- * @version    $Id$
- * @since      Disponible depuis la Release 0.7alpha
- */
-
-/** @ignore */
-require_once('authentication.class.php');
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
  * Default authentication class for galette
  *
- * @name GaletteLogin
- * @package Galette
+ * PHP version 5
  *
+ * Copyright © 2007-2009 The Galette Team
+ *
+ * This file is part of Galette (http://galette.tuxfamily.org).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Authentication
+ * @package   Galette
+ *
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2007-2009 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @version   SVN: $Id$
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.7dev - 2007-07-06
  */
-class GaletteLogin extends Authentication{
-	const TABLE = 'adherents';
-	const PK = 'login_adh';
 
-	/**
-	* Default constructor
-	*/
-	public function __construct(){}
+/** @ignore */
+require_once 'authentication.class.php';
 
-	/**
-	* Logs in user.
-	* @param user user's login
-	* @param passe md5 hashed password
-	* @returns integer state : 
-	* 	'-1' if there were a database error
-	*	'-10' if user cannot login (mistake or user doesn't exists)
-	*	'1' if user were logged in successfully
-	*/
-	public function logIn($user, $passe){
-		global $mdb, $log;
+/**
+ * Default authentication class for galette
+ *
+ * @category  Authentication
+ * @name      GaletteLogin
+ * @package   Galette
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2007-2009 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.7dev - 2007-07-06
+ */
+class GaletteLogin extends Authentication
+{
+    const TABLE = 'adherents';
+    const PK = 'login_adh';
 
-		$requete = 'SELECT id_adh, bool_admin_adh, nom_adh, prenom_adh, mdp_adh, pref_lang, activite_adh FROM ' . PREFIX_DB . self::TABLE . ' WHERE ' . self::PK . '=\'' . $user. '\' AND mdp_adh=\'' . $passe . '\'';
+    /**
+    * Default constructor
+    */
+    public function __construct()
+    {
+    }
 
-		$result = $mdb->query( $requete );
-		if( MDB2::isError($result) )
-			return -1;
-		
-		if($result->numRows() == 0){
-			$log->log('No entry found for login `' . $user . '`', PEAR_LOG_WARNING);
-			return(-10);
-		}else{
-			$log->log('User `' . $user . '` logged in.', PEAR_LOG_DEBUG);
-			$row = $result->fetchRow();
-			$this->id = $row->id_adh;
-			$this->login = $row->login_adh;
-			$this->passe = $row->mdp_adh;
-			$this->admin = $row->bool_admin_adh;
-			$this->name = $row->nom_adh;
-			$this->surname = $row->prenom_adh;
-			$this->lang = $row->pref_lang;
-			$this->active = $row->activite_adh;
-			$this->logged = true;
-			//$this->upLastConn($this->login);
-			return(1);
-		}
-	}
+    /**
+    * Logs in user.
+    *
+    * @param string $user  user's login
+    * @param string $passe md5 hashed password
+    *
+    * @return integer state :
+    *     '-1' if there were a database error
+    *    '-10' if user cannot login (mistake or user doesn't exists)
+    *    '1' if user were logged in successfully
+    */
+    public function logIn($user, $passe)
+    {
+        global $mdb, $log;
 
-	/**
-	* Does this login already exists ?
-	* These function should be used for setting admin login into Preferences
-	* @param user the username
-	* @return true if the username already exists, false otherwise
-	*/
-	public function loginExists($user){
-		global $mdb, $log;
+        $requete = 'SELECT id_adh, bool_admin_adh, nom_adh, prenom_adh, mdp_adh'.
+            ', pref_lang, activite_adh FROM ' . PREFIX_DB . self::TABLE .
+            ' WHERE ' . self::PK . '=\'' . $user. '\' AND mdp_adh=\'' .
+            $passe . '\'';
 
-		$requete = 'SELECT ' . self::PK . ' FROM ' . PREFIX_DB . self::TABLE . ' WHERE ' . self::PK . '=\'' . $user . '\'';
+        $result = $mdb->query($requete);
+        if ( MDB2::isError($result) ) {
+            return -1;
+        }
 
-		/* If an error occurs, we consider that username already exists */
-		$result = $mdb->query( $requete );
-		if( MDB2::isError($result) ){
-			$log->log('Unable to check if username `' . $user . '` already exists. ' . $stmt->getMessage() . '(' . $stmt->getDebugInfo() . ')', PEAR_LOG_WARNING);
-			return true;
-		}
-		if($result->numRows() == 0)
-			return false;
-		else
-			return true;
-	}
+        if ($result->numRows() == 0) {
+            $log->log('No entry found for login `' . $user . '`', PEAR_LOG_WARNING);
+            return(-10);
+        } else {
+            $log->log('User `' . $user . '` logged in.', PEAR_LOG_DEBUG);
+            $row = $result->fetchRow();
+            $this->_id = $row->id_adh;
+            $this->_login = $row->login_adh;
+            $this->_passe = $row->mdp_adh;
+            $this->_admin = $row->bool_admin_adh;
+            $this->_name = $row->nom_adh;
+            $this->_surname = $row->prenom_adh;
+            $this->_lang = $row->pref_lang;
+            $this->_active = $row->activite_adh;
+            $this->_logged = true;
+            //$this->upLastConn($this->login);
+            return(1);
+        }
+    }
+
+    /**
+    * Does this login already exists ?
+    * These function should be used for setting admin login into Preferences
+    *
+    * @param string $user the username
+    *
+    * @return true if the username already exists, false otherwise
+    */
+    public function loginExists($user)
+    {
+        global $mdb, $log;
+
+        $requete = 'SELECT ' . self::PK . ' FROM ' . PREFIX_DB . self::TABLE .
+            ' WHERE ' . self::PK . '=\'' . $user . '\'';
+
+        /* If an error occurs, we consider that username already exists */
+        $result = $mdb->query($requete);
+        if ( MDB2::isError($result) ) {
+            $log->log(
+                'Unable to check if username `' . $user . '` already exists. ' .
+                $stmt->getMessage() . '(' . $stmt->getDebugInfo() . ')',
+                PEAR_LOG_WARNING
+            );
+            return true;
+        }
+        if ($result->numRows() == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 }
 ?>
