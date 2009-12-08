370b54fcc31729265b23339eea0e878b325fc900
Johan Cwiklinski <johan@x-tnd.be>
2009-12-08 22:15:51 +0000

    Applying PEAR Coding Standards (http://pear.php.net/manual/en/standards.php)

--- galette/gestion_textes.php
+++ galette/gestion_textes.php
@@ -1,47 +1,52 @@
 <?php
 
-// Copyright © 2007 John perr
-// Copyright © 2007-2008 Johan Cwiklinski
-//
-// This file is part of Galette (http://galette.tuxfamily.org).
-//
-// Galette is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Galette is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Galette. If not, see <http://www.gnu.org/licenses/>.
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * gestion_textes.php, 16 septembre 2007
+ * Texts management
  *
- * @package    Galette
+ * PHP version 5
  *
- * @author     John Perr
- * @copyright  2007 John Perr
- * @copyright  2007-2008 Johan Cwiklinski
- * @license    http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
- * @version    $Id$
- * @since      Disponible depuis la Release 0.7
+ * Copyright © 2007-2009 The Galette Team
+ *
+ * This file is part of Galette (http://galette.tuxfamily.org).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Main
+ * @package   Galette
+ *
+ * @author    John Perr <johnperr@abul.org>
+ * @copyright 2007-2009 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @version   SVN: $Id$
+ * @link      http://galette.tuxfamily.org
+ * @since     Availaible since 0.7dev - 2007-10-16
  */
 
 /** @ignore */
-require_once('includes/galette.inc.php');
+require_once 'includes/galette.inc.php';
 
-if ( !$login->isLogged() ){
-	header('location: index.php');
-	die();
-}elseif ( !$login->isAdmin() ){
-	header('location: voir_adherent.php');
-	die();
+if ( !$login->isLogged() ) {
+    header('location: index.php');
+    die();
+} elseif ( !$login->isAdmin() ) {
+    header('location: voir_adherent.php');
+    die();
 }
-include(WEB_ROOT . 'classes/texts.class.php');
+
+require_once WEB_ROOT . 'classes/texts.class.php';
 
 // initialize warnings
 $error_detected = array();
@@ -53,28 +58,36 @@ $cur_ref = Texts::DEFAULT_REF;
 $texts = new Texts();
 
 //set the language
-if( isset($_POST['sel_lang']) ){
-	$cur_lang = $_POST['sel_lang'];
+if ( isset($_POST['sel_lang']) ) {
+    $cur_lang = $_POST['sel_lang'];
 }
 //set the text entry
-if( isset($_POST['sel_ref']) ){
-	$cur_ref = $_POST['sel_ref'];
+if ( isset($_POST['sel_ref']) ) {
+    $cur_ref = $_POST['sel_ref'];
 }
 
 if (isset($_POST['valid']) && $_POST['valid'] == '1') {
-	//form was send normally, we try to store new values
-	$res = $texts->setTexts(
-		$cur_ref,
-		$cur_lang,
-		$_POST['text_subject'],
-		$_POST['text_body']
-	);
+    //form was send normally, we try to store new values
+    $res = $texts->setTexts(
+        $cur_ref,
+        $cur_lang,
+        $_POST['text_subject'],
+        $_POST['text_body']
+    );
 
-	if( !$res ){
-		$error_detected[] = preg_replace('(%s)', $mtxt['tcomment'], _T("Email: '%s' has not been modified!"));
-	}else{
-		$warning_detected[] = preg_replace('(%s)', $mtxt['tcomment'], _T("Email: '%s' has been successfully modified."));
- 	}
+    if ( !$res ) {
+        $error_detected[] = preg_replace(
+            '(%s)',
+            $mtxt['tcomment'],
+            _T("Email: '%s' has not been modified!")
+        );
+    } else {
+        $warning_detected[] = preg_replace(
+            '(%s)',
+            $mtxt['tcomment'],
+            _T("Email: '%s' has been successfully modified.")
+        );
+    }
 }
 
 $mtxt = $texts->getTexts($cur_ref, $cur_lang);
