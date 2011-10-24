7dcb0fff912e49bae462d6cc388d00794e45ebd6
Johan Cwiklinski <johan@x-tnd.be>
2011-10-24 17:39:22 +0000

    Filtering on member's activity were not efficient

--- galette/classes/members.class.php
+++ galette/classes/members.class.php
@@ -611,10 +611,10 @@ class Members
             if ( $varslist->account_status_filter ) {
                 switch($varslist->account_status_filter) {
                 case 1:
-                    $select->where('activite_adh=1');
+                    $select->having('activite_adh=1');
                     break;
                 case 2:
-                    $select->where('activite_adh=0');
+                    $select->having('activite_adh=0');
                     break;
                 }
             }
