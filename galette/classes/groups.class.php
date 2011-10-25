3138e6bbbe0c18d5000bc29932a49fd45b95a27a
Johan Cwiklinski <johan@x-tnd.be>
2011-10-25 18:46:57 +0000

    First template of a class to manage groups

---
+++ galette/classes/groups.class.php
@@ -0,0 +1,130 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Groups managment
+ *
+ * PHP version 5
+ *
+ * Copyright © 2011 The Galette Team
+ *
+ * This file is part of Galette (http://galette.tuxfamily.org).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Classes
+ * @package   Galette
+ *
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2011 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @version   SVN: $Id$
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.7dev - 2011-10-25
+ */
+
+/**
+ * This class handles groups, their owners and members.
+ *
+ * @category  Classes
+ * @name      Groups
+ * @package   Galette
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2011 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.7dev - 2011-10-25
+ */
+class Groups
+{
+    private $_id;
+    private $_group_name;
+    private $_owner;
+    private $_members;
+
+    /**
+     * Is current loggedin user owner of the group?
+     *
+     * @return boolean
+     */
+    public function isOwner()
+    {
+        global $login;
+        if ( $login->isAdmin() || $login->isStaff() ) {
+            //admins are groups owners, as well as staff members!
+            return true;
+        } else {
+            //let's check if current uloggedin user is group owner
+            return $this->_owner->login == $login->login;
+        }
+    }
+
+    /**
+     * Can currently loggedin user manage group?
+     *
+     * @return boolean
+     */
+    public function canManage()
+    {
+        global $login;
+        if ( $this->isOwner() ) {
+            return true;
+        } else {
+            /** TODO: check if current loggedin member can manage group */
+        }
+    }
+
+    /**
+     * Get group id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Get group name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->_group_name;
+    }
+
+    /**
+     * Get group owner
+     *
+     * @return Adherent
+     */
+    public function getOwner()
+    {
+        return $this->_owner;
+    }
+
+    /**
+     * Get group members
+     *
+     * @return Adherent[]
+     */
+    public function getMembers()
+    {
+        return $this->_members;
+    }
+}
+
+?>
