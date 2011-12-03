81481debe3dd78063bc8389cb4ca0a694c8f65c1
Johan Cwiklinski <johan@x-tnd.be>
2011-12-03 07:08:40 +0000

    Indent

--- galette/classes/texts.class.php
+++ galette/classes/texts.class.php
@@ -58,105 +58,105 @@ class Texts
 
     private static $_defaults = array(
         array(
-            'tid' => 1,
-            'tref' => 'sub',
-            'tsubject' => 'Your identifiers',
-            'tbody' => "Hello,\r\n\r\nYou've just been subscribed on the members management system of {NAME}.\r\n\r\nIt is now possible to follow in real time the state of your subscription and to update your preferences from the web interface.\r\n\r\nPlease login at this address:\r\n{LOGIN_URI}\r\n\r\nUsername: {LOGIN}\r\nPassword: {PASSWORD}\r\n\r\nSee you soon!\r\n\r\n(this mail was sent automatically)",
-            'tlang'=> 'en_US',
-            'tcomment'=>'New user registration'
+            'tid'       => 1,
+            'tref'      => 'sub',
+            'tsubject'  => 'Your identifiers',
+            'tbody'     => "Hello,\r\n\r\nYou've just been subscribed on the members management system of {NAME}.\r\n\r\nIt is now possible to follow in real time the state of your subscription and to update your preferences from the web interface.\r\n\r\nPlease login at this address:\r\n{LOGIN_URI}\r\n\r\nUsername: {LOGIN}\r\nPassword: {PASSWORD}\r\n\r\nSee you soon!\r\n\r\n(this mail was sent automatically)",
+            'tlang'     => 'en_US',
+            'tcomment'  => 'New user registration'
         ),
         array(
-            'tid' => 2,
-            'tref' => 'sub',
-            'tsubject' => 'Votre adhésion',
-            'tbody' =>"Bonjour,\r\n\r\nVous venez d'adhérer à {NAME}.\r\n\r\nVous pouvez désormais accéder à vos coordonnées et souscriptions en vous connectant à l'adresse suivante :\r\n\r\n{LOGIN_URI} \r\n\r\nIdentifiant : {LOGIN}\r\nMot de passe : {PASSWORD}\r\n\r\nA bientôt!\r\n\r\n(Ce courriel est un envoi automatique)",
-            'tlang'=>'fr_FR',
-            'tcomment'=>'Nouvelle adhésion'
-        ),
-
-        array(
-            'tid' => 4,
-            'tref' => 'pwd',
-            'tsubject' => 'Your identifiers',
-            'tbody' =>"Hello,\r\n\r\nSomeone (probably you) asked to recover your password.\r\n\r\nPlease login at this address to set your new password :\r\n{CHG_PWD_URI}\r\n\r\nUsername: {LOGIN}\r\nThe above link will be valid until {LINK_VALIDITY}.\r\n\r\nSee you soon!\r\n\r\n(this mail was sent automatically)",
-            'tlang'=> 'en_US',
-            'tcomment'=>'Lost password email'
-        ),
-        array(
-            'tid' => 5,
-            'tref' => 'pwd',
-            'tsubject' => 'Vos Identifiants',
-            'tbody' =>"Bonjour,\r\n\r\nQuelqu'un (probablement vous) a demandé la récupération de votre mot de passe.\r\n\r\nConnectez vous à cette adresse pour valider le nouveau mot de passe :\r\n{CHG_PWD_URI}\r\n\r\nIdentifiant : {LOGIN}\r\nLe lien ci-dessus sera valide jusqu'au {LINK_VALIDITY}.\r\n\r\nA Bientôt!\r\n\r\n(Ce courriel est un envoi automatique)",
-            'tlang'=>'fr_FR',
-            'tcomment'=>'Récupération du mot de passe'
+            'tid'       => 2,
+            'tref'      => 'sub',
+            'tsubject'  => 'Votre adhésion',
+            'tbody'     => "Bonjour,\r\n\r\nVous venez d'adhérer à {NAME}.\r\n\r\nVous pouvez désormais accéder à vos coordonnées et souscriptions en vous connectant à l'adresse suivante :\r\n\r\n{LOGIN_URI} \r\n\r\nIdentifiant : {LOGIN}\r\nMot de passe : {PASSWORD}\r\n\r\nA bientôt!\r\n\r\n(Ce courriel est un envoi automatique)",
+            'tlang'     => 'fr_FR',
+            'tcomment'  => 'Nouvelle adhésion'
         ),
 
         array(
-            'tid' => 7,
-            'tref' => 'contrib',
-            'tsubject' => 'Your contribution',
-            'tbody' =>"Your contribution has succefully been taken into account by {NAME}.\r\n\r\nIt is valid until {DEADLINE}.\r\n\r\nYou can now login and browse or modify your personnal data using your galette identifiers.\r\n\r\n{COMMENT}",
-            'tlang'=> 'en_US',
-            'tcomment'=>'Receipt send for every new contribution'
+            'tid'       => 4,
+            'tref'      => 'pwd',
+            'tsubject'  => 'Your identifiers',
+            'tbody'     => "Hello,\r\n\r\nSomeone (probably you) asked to recover your password.\r\n\r\nPlease login at this address to set your new password :\r\n{CHG_PWD_URI}\r\n\r\nUsername: {LOGIN}\r\nThe above link will be valid until {LINK_VALIDITY}.\r\n\r\nSee you soon!\r\n\r\n(this mail was sent automatically)",
+            'tlang'     => 'en_US',
+            'tcomment'  => 'Lost password email'
         ),
         array(
-            'tid' => 8,
-            'tref' => 'contrib',
-            'tsubject' => 'Votre cotisation',
-            'tbody' => "Votre cotisation à {NAME} a été enregistrée et validée par l'association.\r\n\r\nElle est valable jusqu'au {DEADLINE}\r\n\r\nVous pouvez désormais accéder à vos données personnelles à l'aide de vos identifiants galette.\r\n\r\n{COMMENT}",
-            'tlang'=>'fr_FR',
-            'tcomment'=>'Accusé de réception de cotisation'
-        ),
-
-        array(
-            'tid' => 10,
-            'tref' => 'newadh',
-            'tsubject' => 'New registration from {NAME_ADH}',
-            'tbody' =>"{NAME_ADH} has registred on line with login: {LOGIN}",
-            'tlang'=> 'en_US',
-            'tcomment'=>'New registration (sent to admin)'
-        ),
-        array(
-            'tid' => 11,
-            'tref' => 'newadh',
-            'tsubject' => 'Nouvelle inscription de {NAME_ADH}',
-            'tbody' =>"{NAME_ADH} s'est inscrit via l'interface web avec le login {LOGIN}",
-            'tlang'=>'fr_FR',
-            'tcomment'=>'Nouvelle inscription (envoyée a l\'admin)'
+            'tid'       => 5,
+            'tref'      => 'pwd',
+            'tsubject'  => 'Vos Identifiants',
+            'tbody'     => "Bonjour,\r\n\r\nQuelqu'un (probablement vous) a demandé la récupération de votre mot de passe.\r\n\r\nConnectez vous à cette adresse pour valider le nouveau mot de passe :\r\n{CHG_PWD_URI}\r\n\r\nIdentifiant : {LOGIN}\r\nLe lien ci-dessus sera valide jusqu'au {LINK_VALIDITY}.\r\n\r\nA Bientôt!\r\n\r\n(Ce courriel est un envoi automatique)",
+            'tlang'     => 'fr_FR',
+            'tcomment'  => 'Récupération du mot de passe'
         ),
 
         array(
-            'tid' => 13,
-            'tref' => 'newcont',
-            'tsubject' => 'New contribution for {NAME_ADH}',
-            'tbody' =>"The contribution from {NAME_ADH} has been registered (new deadline: {DEADLINE})\r\n\r\n{COMMENT}",
-            'tlang'=> 'en_US',
-            'tcomment'=>'New contribution (sent to admin)'
+            'tid'       => 7,
+            'tref'      => 'contrib',
+            'tsubject'  => 'Your contribution',
+            'tbody'     => "Your contribution has succefully been taken into account by {NAME}.\r\n\r\nIt is valid until {DEADLINE}.\r\n\r\nYou can now login and browse or modify your personnal data using your galette identifiers.\r\n\r\n{COMMENT}",
+            'tlang'     => 'en_US',
+            'tcomment'  => 'Receipt send for every new contribution'
         ),
         array(
-            'tid' => 14,
-            'tref' => 'newcont',
-            'tsubject' => 'Nouvelle contribution de {NAME_ADH}',
-            'tbody' =>"La contribution de {NAME_ADH} a été enregistrée (nouvelle échéance: {DEADLINE})\r\n\r\n{COMMENT}",
-            'tlang'=>'fr_FR',
-            'tcomment'=>'Nouvelle contribution (envoyée a l\'admin)'
+            'tid'       => 8,
+            'tref'      => 'contrib',
+            'tsubject'  => 'Votre cotisation',
+            'tbody'     => "Votre cotisation à {NAME} a été enregistrée et validée par l'association.\r\n\r\nElle est valable jusqu'au {DEADLINE}\r\n\r\nVous pouvez désormais accéder à vos données personnelles à l'aide de vos identifiants galette.\r\n\r\n{COMMENT}",
+            'tlang'     => 'fr_FR',
+            'tcomment'  => 'Accusé de réception de cotisation'
         ),
 
         array(
-            'tid' => 16,
-            'tref' => 'newselfadh',
-            'tsubject' => 'New self registration from {NAME_ADH}',
-            'tbody' =>"{NAME_ADH} has self registred on line with login: {LOGIN}",
-            'tlang'=> 'en_US',
-            'tcomment'=>'New registration (sent to admin)'
+            'tid'       => 10,
+            'tref'      => 'newadh',
+            'tsubject'  => 'New registration from {NAME_ADH}',
+            'tbody'     => "{NAME_ADH} has registred on line with login: {LOGIN}",
+            'tlang'     => 'en_US',
+            'tcomment'  => 'New registration (sent to admin)'
         ),
         array(
-            'tid' => 17,
-            'tref' => 'newselfadh',
-            'tsubject' => 'Nouvelle auto inscription de {NAME_ADH}',
-            'tbody' =>"{NAME_ADH} s'est auto inscrit via l'interface web avec le login {LOGIN}",
-            'tlang'=>'fr_FR',
-            'tcomment'=>'Nouvelle inscription (envoyée a l\'admin)'
+            'tid'       => 11,
+            'tref'      => 'newadh',
+            'tsubject'  => 'Nouvelle inscription de {NAME_ADH}',
+            'tbody'     => "{NAME_ADH} s'est inscrit via l'interface web avec le login {LOGIN}",
+            'tlang'     => 'fr_FR',
+            'tcomment'  => 'Nouvelle inscription (envoyée a l\'admin)'
+        ),
+
+        array(
+            'tid'       => 13,
+            'tref'      => 'newcont',
+            'tsubject'  => 'New contribution for {NAME_ADH}',
+            'tbody'     => "The contribution from {NAME_ADH} has been registered (new deadline: {DEADLINE})\r\n\r\n{COMMENT}",
+            'tlang'     => 'en_US',
+            'tcomment'  => 'New contribution (sent to admin)'
+        ),
+        array(
+            'tid'       => 14,
+            'tref'      => 'newcont',
+            'tsubject'  => 'Nouvelle contribution de {NAME_ADH}',
+            'tbody'     => "La contribution de {NAME_ADH} a été enregistrée (nouvelle échéance: {DEADLINE})\r\n\r\n{COMMENT}",
+            'tlang'     => 'fr_FR',
+            'tcomment'  => 'Nouvelle contribution (envoyée a l\'admin)'
+        ),
+
+        array(
+            'tid'       => 16,
+            'tref'      => 'newselfadh',
+            'tsubject'  => 'New self registration from {NAME_ADH}',
+            'tbody'     => "{NAME_ADH} has self registred on line with login: {LOGIN}",
+            'tlang'     => 'en_US',
+            'tcomment'  => 'New registration (sent to admin)'
+        ),
+        array(
+            'tid'       => 17,
+            'tref'      => 'newselfadh',
+            'tsubject'  => 'Nouvelle auto inscription de {NAME_ADH}',
+            'tbody'     => "{NAME_ADH} s'est auto inscrit via l'interface web avec le login {LOGIN}",
+            'tlang'     => 'fr_FR',
+            'tcomment'  => 'Nouvelle inscription (envoyée a l\'admin)'
         ),
 
     );
