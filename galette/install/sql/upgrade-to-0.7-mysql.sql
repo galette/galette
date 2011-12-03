6adf385c97f1aa088184c13803a34e8c35d6c5d8
Johan Cwiklinski <johan@x-tnd.be>
2011-12-03 09:28:47 +0000

    Fix a syntax issue

--- galette/install/sql/upgrade-to-0.7-mysql.sql
+++ galette/install/sql/upgrade-to-0.7-mysql.sql
@@ -176,8 +176,8 @@ ALTER TABLE galette_dynamic_fields ADD FOREIGN KEY (field_id)
 ALTER TABLE galette_cotisations ADD type_paiement_cotis TINYINT( 3 ) unsigned NOT NULL DEFAULT '0' AFTER montant_cotis;
 
 -- Fix round issues
-ALTER TABLE galette_cotisations CHANGE montant_cotis montant_cotis DECIMAL( 15, 2 ) UNSIGNED NULL DEFAULT '0'
-ALTER TABLE galette_transactions CHANGE trans_amount trans_amount DECIMAL( 15, 2 ) NULL DEFAULT '0' 
+ALTER TABLE galette_cotisations CHANGE montant_cotis montant_cotis DECIMAL( 15, 2 ) UNSIGNED NULL DEFAULT '0';
+ALTER TABLE galette_transactions CHANGE trans_amount trans_amount DECIMAL( 15, 2 ) NULL DEFAULT '0' ;
 
 -- Put back foreign keys
 SET FOREIGN_KEY_CHECKS=1;
