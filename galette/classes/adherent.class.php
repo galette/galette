c7e7c67498baaafbef92500019ba137b2d78bbd1
Johan Cwiklinski <johan@x-tnd.be>
2012-02-20 20:07:33 +0100

    Revert "Fix bug #19463"; that causes more errors than it repairs
    This reverts commit 61230016d0ae46b58ec598ed7cd58cf05f676e49.

--- galette/classes/adherent.class.php
+++ galette/classes/adherent.class.php
@@ -998,10 +998,12 @@ class Adherent
                     // dates
                     case 'date_crea_adh':
                     case 'ddn_adh':
-                        try {
-                            $d = new DateTime($value);
-                            $this->$prop = $d->format(_T("Y-m-d"));
-                        } catch (Exception $e) {
+                        /** FIXME: only ok dates dd/mm/yyyy */
+                        if ( !preg_match(
+                            '@^([0-9]{2})/([0-9]{2})/([0-9]{4})$@',
+                            $value,
+                            $date
+                        ) ) {
                             $log->log(
                                 'Wrong date format. field: ' . $key .
                                 ', value: ' . $value . ' | ' . $e->getMessage(),
@@ -1013,11 +1015,18 @@ class Adherent
                                     '%field'
                                 ),
                                 array(
-                                    _T("Y-m-d"),
+                                    /*_T("Y-m-d")*/'dd/mm/yyy',
                                     $this->_fields[$key]['label']
                                 ),
                                 _T("- Wrong date format (%date_format) for %field!")
                             );
+                        } else {
+                            if ( !checkdate($date[2], $date[1], $date[3]) ) {
+                                $errors[] = _T("- Non valid date!");
+                            } else {
+                                $this->$prop = $date[3] . '-' . $date[2] . '-' .
+                                    $date[1];
+                            }
                         }
                         break;
                     case 'email_adh':
