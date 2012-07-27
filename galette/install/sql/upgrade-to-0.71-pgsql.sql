04ad18e81d61e9615b9023cc5e8bc3e2ba4869e0
Johan Cwiklinski <johan@x-tnd.be>
2012-07-27 06:55:23 +0200

    oops, field_required has been modified instead of field_repeat! refs #305

--- galette/install/sql/upgrade-to-0.71-pgsql.sql
+++ galette/install/sql/upgrade-to-0.71-pgsql.sql
@@ -5,8 +5,8 @@ ALTER TABLE galette_adherents ALTER COLUMN prenom_adh TYPE varchar(50);
 ALTER TABLE galette_adherents ALTER COLUMN societe_adh TYPE varchar(200);
 ALTER TABLE galette_transactions ALTER COLUMN trans_desc TYPE varchar(150);
 
-ALTER TABLE galette_field_types ALTER COLUMN field_required DROP DEFAULT;
-ALTER TABLE galette_field_types ALTER field_required TYPE integer USING CASE WHEN field_required=false THEN NULL ELSE 0 END;
-ALTER TABLE galette_field_types ALTER COLUMN field_required SET DEFAULT NULL;
+ALTER TABLE galette_field_types ALTER COLUMN field_repeat DROP DEFAULT;
+ALTER TABLE galette_field_types ALTER field_repeat TYPE integer USING CASE WHEN field_repeat=false THEN NULL ELSE 0 END;
+ALTER TABLE galette_field_types ALTER COLUMN field_repeat SET DEFAULT NULL;
 
 UPDATE galette_database SET version = 0.701;
