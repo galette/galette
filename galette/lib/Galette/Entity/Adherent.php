9cf1ed8c9545f43bfd470aadfdec0fe32dc93ad2
Johan Cwiklinski <johan@x-tnd.be>
2012-12-14 17:41:29 +0100

    Bad logger level

--- galette/lib/Galette/Entity/Adherent.php
+++ galette/lib/Galette/Entity/Adherent.php
@@ -1416,7 +1416,7 @@ class Adherent
                         $log->log(
                             'Bad date (' . $this->$rname . ') | ' .
                             $e->getMessage(),
-                            PER_LOG_INFO
+                            KLogger::INFO
                         );
                         return $this->$rname;
                     }
