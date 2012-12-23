3c6185be0f88c2f9cd50d6324d20b6fe261e0ea0
Johan Cwiklinski <johan@x-tnd.be>
2012-12-23 13:53:55 +0100

    Bump phpMailer version

--- galette/config/versions.inc.php
+++ galette/config/versions.inc.php
@@ -41,5 +41,5 @@ define('TCPDF_VERSION', '5.9.202');
 define('JQUERY_VERSION', '1.6.2');
 define('JQUERY_UI_VERSION', '1.8.14');
 define('JQUERY_MARKITUP_VERSION', '1.1.12');
-define('PHP_MAILER_VERSION', '5.2.1');
+define('PHP_MAILER_VERSION', '5.2.2');
 define('GAPI_VERSION', '0.4.6');
