fc4b0531f10c068be68e12acce462a2423728d69
Johan Cwiklinski <johan@x-tnd.be>
2013-01-28 08:26:17 +0100

    Typos in mysql upgrade script

--- galette/install/sql/upgrade-to-0.74-mysql.sql
+++ galette/install/sql/upgrade-to-0.74-mysql.sql
@@ -11,10 +11,10 @@ INSERT INTO galette_title(short_label, long_label) VALUES ('Mr.', 'Mister');
 INSERT INTO galette_title(short_label, long_label) VALUES ('Mrs.', 'Mrs.');
 INSERT INTO galette_title(short_label, long_label) VALUES ('Miss', 'Miss');
 
-ALTER TABLE galette_adherents CHANGE titre_adh titre_adh int(10) NOT NULL DEFAULT '';
+ALTER TABLE galette_adherents CHANGE titre_adh titre_adh int(10) NOT NULL DEFAULT 0;
 ALTER TABLE galette_adherents CHANGE mdp_adh mdp_adh VARCHAR( 60 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '';
 
-ALTER TABLE galette_adherents ADD sexe_adh_adh TINYINT(1) DEFAULT 0;
+ALTER TABLE galette_adherents ADD sexe_adh TINYINT(1) DEFAULT 0;
 UPDATE galette_adherents SET sexe_adh = titre_adh;
 
 UPDATE galette_database SET version = 0.702;
