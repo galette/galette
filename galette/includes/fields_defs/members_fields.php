f99bbadd84ea983f6311bd84189f968167be86cc
Johan Cwiklinski <johan@x-tnd.be>
2013-06-02 11:55:07 +0200

    Fix authors and copyright date

--- galette/includes/fields_defs/members_fields.php
+++ galette/includes/fields_defs/members_fields.php
@@ -27,10 +27,8 @@
  * @category  Functions
  * @package   Galette
  *
- * @author    Frédéric Jaqcuot <unknown@unknow.com>
- * @author    Georges Khaznadar (password encryption, images) <unknown@unknow.com>
  * @author    Johan Cwiklinski <johan@x-tnd.be>
- * @copyright 2003-2013 The Galette Team
+ * @copyright 2012-2013 The Galette Team
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
  * @version   SVN: $Id$
  * @link      http://galette.tuxfamily.org
