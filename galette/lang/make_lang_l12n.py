c284dff57feec80019d7eb38fbd4371fbf26279a
Johan Cwiklinski <johan@x-tnd.be>
2013-09-08 18:24:23 +0200

    Update xgettext.py and make_lang_l12n.py licenses; fixes #704

--- galette/lang/make_lang_l12n.py
+++ galette/lang/make_lang_l12n.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 """
-/* make_lang_l12n.py
- * - Identification
- * Copyright (c) 2004 Georges Khaznadar
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Galette. If not, see <http://www.gnu.org/licenses/>.
- *
- */
+Copyright © 2004-2013 The Galette Team
+
+This file is part of Galette (http://galette.tuxfamily.org).
+
+Galette is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Galette is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Galette. If not, see <http://www.gnu.org/licenses/>.
+
+@author Georges Khaznadar <unknown@unknow.com>
+@author Johan Cwiklinski <johan@x-tnd.be>
 """
 import re
 
