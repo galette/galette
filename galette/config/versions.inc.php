ff9d575a9b4d928732ee52a6577ce787cf14e777
Johan Cwiklinski <johan@x-tnd.be>
2013-12-22 11:29:13 +0100

    Lastest Zend, Smarty and TCPDF

--- galette/config/versions.inc.php
+++ galette/config/versions.inc.php
@@ -35,10 +35,10 @@
  * @link      http://galette.tuxfamily.org
  * @since     Available since 0.7dev - 2009-03-13
  */
-define('SMARTY_VERSION', '3.1.14');
-define('ZEND_VERSION', '2.2.4');
+define('SMARTY_VERSION', '3.1.16');
+define('ZEND_VERSION', '2.2.5');
 define('ANALOG_VERSION', '1.0.0.git876d8a3bb');
-define('TCPDF_VERSION', '6.0.020');
+define('TCPDF_VERSION', '6.0.052');
 define('JQUERY_VERSION', '1.10.2');
 define('JQUERY_MIGRATE_VERSION', '1.2.1');
 define('JQUERY_UI_VERSION', '1.10.3');
