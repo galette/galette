5cafc586a36ace225c189db1dafaa2a476e3b452
Johan Cwiklinski <johan@x-tnd.be>
2014-01-01 19:52:38 +0100

    Check very minimal PHP version to run installer; refs #448

---
+++ galette/install/index.php
@@ -0,0 +1,53 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Check server configuration for Galette
+ *
+ * PHP version 5
+ *
+ * Copyright © 2014 The Galette Team
+ *
+ * This file is part of Galette (http://galette.tuxfamily.org).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Main
+ * @package   Galette
+ *
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2013 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @version   SVN: $Id$
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.8 - 2014-01-01
+ */
+
+// check required PHP version...
+if ( version_compare(PHP_VERSION, '5.3', '>') ) {
+    header('location: installer.php');
+    die();
+}
+?>
+ <html>
+    <head>
+        <title>Galette compatibility check</title>
+    </head>
+    <body>
+        <h1>Galette configuration check</h1>
+        <h2 class="error">Not compatible :(</h2>
+    </body>
+</html>
+
