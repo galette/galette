a2c8d598473975c884cdaa241d072ee6aaab5d77
Johan Cwiklinski <johan@x-tnd.be>
2016-09-24 09:49:06 +0200

    Add local file support for test configuration files

--- tests/config/mysql/config.inc.php
+++ tests/config/mysql/config.inc.php
@@ -1,8 +1,23 @@
 <?php
 define("TYPE_DB", "mysql");
-define("HOST_DB", "localhost");
-define("PORT_DB", "3306");
-define("USER_DB", "root");
-define("PWD_DB", "");
-define("NAME_DB", "galette_tests");
-define("PREFIX_DB", "galette_");
+if (file_exists(__DIR__ . '/local_config.inc.php')) {
+    include_once __DIR__ . '/local_config.inc.php';
+}
+if (!defined('HOST_DB')) {
+    define("HOST_DB", "localhost");
+}
+if (!defined('PORT_DB')) {
+    define("PORT_DB", "3306");
+}
+if (!defined('USER_DB')) {
+    define("USER_DB", "root");
+}
+if (!defined('PWD_DB')) {
+    define("PWD_DB", "");
+}
+if (!defined('NAME_DB')) {
+    define("NAME_DB", "galette_tests");
+}
+if (!defined('PREFIX_DB')) {
+    define("PREFIX_DB", "galette_");
+}
