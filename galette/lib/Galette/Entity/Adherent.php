galette/galette

commit cd8da6f0849734ab2af065c5bbb1e75c55931857

Fix various MySQL 5.7 strict mode issues

galette/lib/Galette/Entity/Adherent.php
@@ -1219,7 +1219,7 @@ class Adherent
                         && $this->$prop === false
                     ) {
                         //Handle booleans for postgres ; bugs #18899 and #19354
-                        $values[$field] = 'false';
+                        $values['field'] = $this->zdb->isPostgres() ? 'false' : 0;
                     } elseif ($field === 'parent_id') {
                         //handle parents
                         if ($this->_parent === null) {

galette/lib/Galette/Core/MailingHistory.php
@@ -284,7 +284,9 @@ class MailingHistory extends History
                 'mailing_body' => $this->_message,
                 'mailing_date' => $this->_date,
                 'mailing_recipients' => serialize($_recipients),
-                'mailing_sent' => ($this->_sent) ? true : 'false'
+                'mailing_sent' => ($this->_sent) ?
+                    true :
+                    ($this->zdb->isPostgres() ? 'false' : 0)
             );
 
             $update = $zdb->update(self::TABLE);
@@ -331,7 +333,9 @@ class MailingHistory extends History
                 'mailing_body' => $this->_message,
                 'mailing_date' => $this->_date,
                 'mailing_recipients' => serialize($_recipients),
-                'mailing_sent' => ($this->_sent) ? true : 'false'
+                'mailing_sent' => ($this->_sent) ?
+                    true :
+                    ($this->zdb->isPostgres() ? 'false' : 0)
             );
 
             $insert = $zdb->insert(self::TABLE);

galette/lib/Galette/Entity/FieldsConfig.php
@@ -40,6 +40,7 @@ namespace Galette\Entity;
 use Analog\Analog;
 use Zend\Db\Adapter\Adapter;
 use Galette\Core\Db;
+use Zend\Db\Sql\Expression;
 
 /**
  * Fields config class for galette :
@@ -298,7 +299,7 @@ class FieldsConfig
                         );
                         $required = $f['required'];
                         if ( $required === false ) {
-                            $required = 'false';
+                            $required = $this->zdb->isPostgres() ? 'false' : 0;
                         }
                         $params[] = array(
                             'field_id'    => $k,
@@ -312,7 +313,7 @@ class FieldsConfig
                 }
 
                 if ( count($params) > 0 ) {
-                    $this->_insert($this->zdb, $params);
+                    $this->_insert($params);
                     $this->load();
                 }
             }
@@ -353,7 +354,7 @@ class FieldsConfig
                 //build default config for each field
                 $required = $this->_defaults[$f]['required'];
                 if ( $required === false ) {
-                    $required = 'false';
+                    $required = $this->zdb->isPostgres() ? 'false' : 0;
                 }
                 $params[] = array(
                     'field_id'    => $f,
@@ -653,7 +654,7 @@ class FieldsConfig
             foreach ( $this->_categorized_fields as $cat ) {
                 foreach ( $cat as $pos=>$field ) {
                     if ( in_array($field['field_id'], $this->_non_required) ) {
-                        $field['required'] = 'false';
+                        $field['required'] = $this->zdb->isPostgres() ? 'false' : 0;
                     }
                     $params = array(
                         'required'  => $field['required'],
@@ -744,7 +745,9 @@ class FieldsConfig
                 /** Why where parameter is named where1 ?? */
                 $stmt->execute(
                     array(
-                        'required'  => ($or->required === false) ?  'false' : true,
+                        'required'  => ($or->required === false) ?
+                            ($this->zdb->isPostgres() ? 'false' : 0) :
+                            true,
                         'where1'    => $or->field_id
                     )
                 );
@@ -800,7 +803,6 @@ class FieldsConfig
             )
         );
         $stmt = $this->zdb->sql->prepareStatementForSqlObject($insert);
-
         foreach ( $values as $d ) {
             $stmt->execute(
                 array(

galette/lib/Galette/Entity/Reminder.php
@@ -163,8 +163,12 @@ class Reminder
             'reminder_type'     => $this->_type,
             'reminder_dest'     => $this->_dest->id,
             'reminder_date'     => $now->format('Y-m-d'),
-            'reminder_success'  => ($this->_success) ? true : 'false',
-            'reminder_nomail'   => ($this->_nomail) ? true : 'false'
+            'reminder_success'  => ($this->_success) ?
+                true :
+                ($this->zdb->isPostgres() ? 'false' : 0),
+            'reminder_nomail'   => ($this->_nomail) ?
+                true :
+                ($this->zdb->isPostgres() ? 'false' : 0)
         );
         try {
             $insert = $zdb->insert(self::TABLE);

commit 2765d753a160934645675a48dd2b98c4059fbced

Fix regression introduced in cd8da6f0849734ab2af065c5bbb1e75c55931857

galette/lib/Galette/Entity/Adherent.php
@@ -1219,7 +1219,7 @@ class Adherent
                         && $this->$prop === false
                     ) {
                         //Handle booleans for postgres ; bugs #18899 and #19354
-                        $values['field'] = $this->zdb->isPostgres() ? 'false' : 0;
+                        $values[$field] = $this->zdb->isPostgres() ? 'false' : 0;
                     } elseif ($field === 'parent_id') {
                         //handle parents
                         if ($this->_parent === null) {