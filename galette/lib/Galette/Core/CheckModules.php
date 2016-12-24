a6d10bc001e4e27791732d304bcd2c803194e8ef
Johan Cwiklinski <johan@x-tnd.be>
2016-12-24 09:39:34 +0100

    mcrypt ext is not needed from a while

--- galette/lib/Galette/Core/CheckModules.php
+++ galette/lib/Galette/Core/CheckModules.php
@@ -83,13 +83,6 @@ class CheckModules
             $this->_good['gd'] = str_replace('%s', 'gd', _T("'%s' module"));
         }
 
-        //mcrypt module is required
-        if (!extension_loaded('mcrypt')) {
-            $this->_missing[] = str_replace('%s', 'mcrypt', _T("'%s' module"));
-        } else {
-            $this->_good['mcrypt'] = str_replace('%s', 'mcrypt', _T("'%s' module"));
-        }
-
         //one of mysql or pgsql driver must be present
         if ( !extension_loaded('pdo_mysql')
             && !extension_loaded('pdo_pgsql')
