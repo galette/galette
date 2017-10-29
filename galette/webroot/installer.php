ac0a3fb4cb3358c1354ab49237120fc04cf7a742
Johan Cwiklinski <johan@x-tnd.be>
2017-10-29 08:50:04 +0100

    Fix language change from installer

--- galette/webroot/installer.php
+++ galette/webroot/installer.php
@@ -59,8 +59,12 @@ if (isset($session['lang'])) {
     }
 } else {
     $i18n = new Galette\Core\I18n();
-    $session['lang'] = serialize($i18n);
 }
+
+if (isset($_GET['pref_lang'])) {
+    $i18n->changeLanguage($_GET['pref_lang']);
+}
+$session['lang'] = serialize($i18n);
 
 require_once '../includes/i18n.inc.php';
 
