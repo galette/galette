a5a0fdc1d6576f297a73e0e809c46d23987fb37e
Johan Cwiklinski <johan@x-tnd.be>
2019-01-27 11:32:39 +0100

    Bump version

--- galette/includes/galette.inc.php
+++ galette/includes/galette.inc.php
@@ -104,7 +104,7 @@ if (defined('GALETTE_XHPROF_PATH')
     $profiler->start();
 }
 
-define('GALETTE_VERSION', 'v0.9.2');
+define('GALETTE_VERSION', 'v0.9.2.1');
 define('GALETTE_COMPAT_VERSION', '0.9.2');
 define('GALETTE_DB_VERSION', '0.920');
 if (!defined('GALETTE_MODE')) {
