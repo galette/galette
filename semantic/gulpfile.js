d2bd832d979252cf257943e8f60eb5082bfca2d6
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-11-23 12:44:36 +0100

    Do not include huge semantic files on git repo
    Required resources will be copied from node_modules
    if missing when running npx run gulp from semantic dir.

--- semantic/gulpfile.js
+++ semantic/gulpfile.js
@@ -1,4 +1,29 @@
 /*******************************
+ * Install files removed on git
+ *******************************/
+
+var fs = require('fs-extra'); // Used for recursive copying
+var path = require('path');
+
+var _missing = [
+    'src/definitions',
+    'src/semantic.less',
+    'src/theme.less',
+    'src/themes'
+];
+
+_missing.forEach(function (item){
+    try {
+        var stat = fs.statSync(path.join(__dirname, './', item));
+        console.log('\'' + item + '\' folder already exists. Continuing.')
+    } catch (e) {
+        console.log('Copying \'' + item + '\' folder from \'node_modules/fomantic-ui/' + item + '\'');
+        fs.copySync(path.join(__dirname, '../node_modules/fomantic-ui/', item), path.join(__dirname, './', item));
+        console.log('Copying done! Continuing.');
+    }
+});
+
+/*******************************
  *           Set-up
  *******************************/
 
