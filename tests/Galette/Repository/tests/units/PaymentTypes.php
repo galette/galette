b4ed7c317e7a1852c454663f1e77d2556cd1d260
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-12-15 15:26:06 +0100

    Handle sequence on PostgreSQL for payment types; refs #1374 refs #1158
    Add unit tests

---
+++ tests/Galette/Repository/tests/units/PaymentTypes.php
@@ -0,0 +1,154 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Payment types repository tests
+ *
+ * PHP version 5
+ *
+ * Copyright © 2019 The Galette Team
+ *
+ * This file is part of Galette (http://galette.tuxfamily.org).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Repository
+ * @package   GaletteTests
+ *
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2019 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @version   SVN: $Id$
+ * @link      http://galette.tuxfamily.org
+ * @since     2019-12-17
+ */
+
+namespace Galette\Repository\test\units;
+
+use \atoum;
+
+/**
+ * Payment types repository tests
+ *
+ * @category  Repository
+ * @name      PaymentTypes
+ * @package   GaletteTests
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2019 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @link      http://galette.tuxfamily.org
+ * @since     2019-12-17
+ */
+class PaymentTypes extends atoum
+{
+    private $zdb;
+    private $preferences;
+    private $session;
+    private $login;
+    private $remove = [];
+    private $i18n;
+
+    /**
+     * Set up tests
+     *
+     * @param string $testMethod Calling method
+     *
+     * @return void
+     */
+    public function beforeTestMethod($testMethod)
+    {
+        $this->zdb = new \Galette\Core\Db();
+        $this->preferences = new \Galette\Core\Preferences($this->zdb);
+        $this->i18n = new \Galette\Core\I18n(
+            \Galette\Core\I18n::DEFAULT_LANG
+        );
+        $this->session = new \RKA\Session();
+        $this->login = new \Galette\Core\Login($this->zdb, $this->i18n, $this->session);
+
+        $types = new \Galette\Repository\PaymentTypes($this->zdb, $this->preferences, $this->login);
+        $res = $types->installInit(false);
+        $this->boolean($res)->isTrue();
+    }
+
+    /**
+     * Tear down tests
+     *
+     * @param string $testMethod Calling method
+     *
+     * @return void
+     */
+    public function afterTestMethod($testMethod)
+    {
+        $this->deletePaymentType();
+    }
+
+    /**
+     * Delete payment type
+     *
+     * @return void
+     */
+    private function deletePaymentType()
+    {
+        if (is_array($this->remove) && count($this->remove) > 0) {
+            $delete = $this->zdb->delete(\Galette\Entity\PaymentType::TABLE);
+            $delete->where->in(\Galette\Repository\PaymentTypes::PK, $this->remove);
+            $this->zdb->execute($delete);
+        }
+
+        //Clean logs
+        $this->zdb->db->query(
+            'TRUNCATE TABLE ' . PREFIX_DB . \Galette\Core\History::TABLE,
+            \Zend\Db\Adapter\Adapter::QUERY_MODE_EXECUTE
+        );
+    }
+
+    /**
+     * Test getList
+     *
+     * @return void
+     */
+    public function testGetList()
+    {
+        $types = new \Galette\Repository\PaymentTypes($this->zdb, $this->preferences, $this->login);
+
+        $list = $types->getList();
+        $this->array($list)->hasSize(6);
+
+        if ($this->zdb->isPostgres()) {
+            $select = $this->zdb->select(\Galette\Entity\PaymentType::TABLE . '_id_seq');
+            $select->columns(['last_value']);
+            $results = $this->zdb->execute($select);
+            $result = $results->current();
+            $this->integer($result->last_value)->isGreaterThanOrEqualTo(6, 'Incorrect payments types sequence');
+        }
+
+        //reinstall payment types
+        $types->installInit();
+
+        $list = $types->getList();
+        $this->array($list)->hasSize(6);
+
+        if ($this->zdb->isPostgres()) {
+            $select = $this->zdb->select(\Galette\Entity\PaymentType::TABLE . '_id_seq');
+            $select->columns(['last_value']);
+            $results = $this->zdb->execute($select);
+            $result = $results->current();
+            $this->integer($result->last_value)->isGreaterThanOrEqualTo(
+                6,
+                'Incorrect payment types sequence ' . $result->last_value
+            );
+        }
+    }
+}
