81c55b4a30f7c7b51d08ca1f3fd47f46035dfcf8
Johan Cwiklinski <johan@x-tnd.be>
2020-07-04 10:42:34 +0200

    Bump version

--- galette/includes/galette.inc.php
+++ galette/includes/galette.inc.php
@@ -108,7 +108,7 @@ if (
 }
 
 define('GALETTE_NIGHTLY', false);
-define('GALETTE_VERSION', 'v0.9.4-dev');
+define('GALETTE_VERSION', 'v0.9.4');
 
 //Version to display
 if (!defined('GALETTE_HIDE_VERSION')) {
