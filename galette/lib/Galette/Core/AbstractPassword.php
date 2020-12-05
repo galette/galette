cf7005b62d6e1e98c0d19d2aef32b759dc487e28
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2020-12-05 10:09:02 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- galette/lib/Galette/Core/AbstractPassword.php
+++ galette/lib/Galette/Core/AbstractPassword.php
@@ -85,7 +85,7 @@ abstract class AbstractPassword
         $pass = '';
         $i = 0;
         while ($i <= $size - 1) {
-            $num = mt_rand(0, strlen($this->chars) - 1)  % strlen($this->chars);
+            $num = mt_rand(0, strlen($this->chars) - 1) % strlen($this->chars);
             $pass .= substr($this->chars, $num, 1);
             $i++;
         }
