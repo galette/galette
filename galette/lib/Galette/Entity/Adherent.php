3e7b22968506313039554cacc601d78ad8080e01
Johan Cwiklinski <johan@x-tnd.be>
2022-07-04 21:54:43 +0200

    Fix check

--- galette/lib/Galette/Entity/Adherent.php
+++ galette/lib/Galette/Entity/Adherent.php
@@ -1192,7 +1192,7 @@ class Adherent
             $this->_parent = null;
         }
 
-        if ($login->isGroupManager() && !$login->isAdmin() && !$login->isStaff() && $login->id != $this->parent_id) {
+        if ($login->isGroupManager() && !$login->isAdmin() && !$login->isStaff() && $this->parent_id !== $login->id) {
             if (!isset($values['groups_adh'])) {
                 $this->errors[] = _T('You have to select a group you own!');
             } else {
