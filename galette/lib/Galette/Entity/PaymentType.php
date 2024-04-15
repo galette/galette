5499ca2d76dd9e9794978a60205b3b8b59f6bf63
Manuel <42542452+manuelh78@users.noreply.github.com>
2024-04-15 21:33:32 +0200

    Update PaymentType.php

--- galette/lib/Galette/Entity/PaymentType.php
+++ galette/lib/Galette/Entity/PaymentType.php
@@ -27,6 +27,7 @@ use Galette\Core\Db;
 use Analog\Analog;
 use Galette\Features\I18n;
 use Galette\Features\Translatable;
+use Galette\Entity\Base\EntityFromDb;
 
 /**
  * Payment type
@@ -37,16 +38,13 @@ use Galette\Features\Translatable;
  * @property string $name
  */
 
-class PaymentType
+class PaymentType extends EntityFromDb
 {
-    use Translatable;
+    //use Translatable;
     use I18n;
 
     public const TABLE = 'paymenttypes';
     public const PK = 'type_id';
-
-    private Db $zdb;
-    private int $id;
 
     public const SCHEDULED = 7;
     public const OTHER = 6;
@@ -56,108 +54,30 @@ class PaymentType
     public const TRANSFER = 4;
     public const PAYPAL = 5;
 
-    /**
-     * Main constructor
-     *
-     * @param Db                                      $zdb  Database instance
-     * @param ArrayObject<string,int|string>|int|null $args Arguments
-     */
+
     public function __construct(Db $zdb, ArrayObject|int $args = null)
     {
-        $this->zdb = $zdb;
-        if (is_int($args)) {
-            $this->load($args);
-        } elseif ($args instanceof ArrayObject) {
-            $this->loadFromRs($args);
-        }
-    }
-
-    /**
-     * Load a payment type from its identifier
-     *
-     * @param integer $id Identifier
-     *
-     * @return bool
-     */
-    public function load(int $id): bool
-    {
-        try {
-            $select = $this->zdb->select(self::TABLE);
-            $select->limit(1)->where([self::PK => $id]);
-
-            $results = $this->zdb->execute($select);
-            $res = $results->current();
-
-            $this->id = $id;
-            $this->name = $res->type_name;
-            return true;
-        } catch (Throwable $e) {
-            Analog::log(
-                'An error occurred loading payment type #' . $id . "Message:\n" .
-                $e->getMessage(),
-                Analog::ERROR
-            );
-            throw $e;
-        }
-    }
-
-    /**
-     * Load payment type from a db ResultSet
-     *
-     * @param ArrayObject<string, int|string> $rs ResultSet
-     *
-     * @return void
-     */
-    private function loadFromRs(ArrayObject $rs): void
-    {
-        $pk = self::PK;
-        $this->id = $rs->$pk;
-        $this->name = $rs->type_name;
-    }
-
-    /**
-     * Store payment type in database
-     *
-     * @return boolean
-     */
-    public function store(): bool
-    {
-        $data = array(
-            'type_name' => $this->name
+        parent::__construct(
+            $zdb,
+            [
+                'table' => self::TABLE,
+                'id' => self::PK,
+                'name' => 'type_name',
+            ],
+            [
+                'toString' => 'name',
+                'name:warningnoempty' => true,
+                'i18n' => ['name']
+            ],
+            $args
         );
-        try {
-            if (isset($this->id) && $this->id > 0) {
-                if ($this->old_name !== null) {
-                    $this->deleteTranslation($this->old_name);
-                    $this->addTranslation($this->name);
-                }
-
-                $update = $this->zdb->update(self::TABLE);
-                $update->set($data)->where([self::PK => $this->id]);
-                $this->zdb->execute($update);
-            } else {
-                $insert = $this->zdb->insert(self::TABLE);
-                $insert->values($data);
-                $add = $this->zdb->execute($insert);
-                if (!$add->count() > 0) {
-                    Analog::log('Not stored!', Analog::ERROR);
-                    return false;
-                }
-
-                $this->id = $this->zdb->getLastGeneratedValue($this);
-
-                $this->addTranslation($this->name);
-            }
-            return true;
-        } catch (Throwable $e) {
-            Analog::log(
-                'An error occurred storing payment type: ' . $e->getMessage() .
-                "\n" . print_r($data, true),
-                Analog::ERROR
-            );
-            throw $e;
-        }
     }
+
+    public function getName($translated = true): string
+    {
+        return $this->getValue('name', $translated);
+    }
+
 
     /**
      * Remove current title
@@ -166,104 +86,15 @@ class PaymentType
      */
     public function remove(): bool
     {
-        $id = (int)$this->id;
+        $id = (int) $this->id;
         if ($this->isSystemType()) {
             throw new \RuntimeException(_T("You cannot delete system payment types!"));
         }
-
-        try {
-            $delete = $this->zdb->delete(self::TABLE);
-            $delete->where([self::PK => $id]);
-            $this->zdb->execute($delete);
-            $this->deleteTranslation($this->name);
-            Analog::log(
-                'Payment type #' . $id . ' (' . $this->name
-                . ') deleted successfully.',
-                Analog::INFO
-            );
-            return true;
-        } catch (Throwable $e) {
-            Analog::log(
-                'Unable to delete payment type ' . $id . ' | ' . $e->getMessage(),
-                Analog::ERROR
-            );
-            throw $e;
-        }
+        $ok = parent::remove();
+        return $ok;
     }
 
-    /**
-     * Getter
-     *
-     * @param string $name Property name
-     *
-     * @return mixed
-     */
-    public function __get(string $name): mixed
-    {
-        switch ($name) {
-            case 'id':
-            case 'name':
-                return $this->$name;
-        }
 
-        throw new \RuntimeException(
-            sprintf(
-                'Unable to get property "%s::%s"!',
-                __CLASS__,
-                $name
-            )
-        );
-    }
-
-    /**
-     * Isset
-     * Required for twig to access properties via __get
-     *
-     * @param string $name Property name
-     *
-     * @return bool
-     */
-    public function __isset(string $name): bool
-    {
-        switch ($name) {
-            case 'id':
-            case 'name':
-                return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Setter
-     *
-     * @param string $name  Property name
-     * @param mixed  $value Property value
-     *
-     * @return void
-     */
-    public function __set(string $name, mixed $value): void
-    {
-        switch ($name) {
-            case 'name':
-                if (trim($value) === '') {
-                    Analog::log(
-                        'Name cannot be empty',
-                        Analog::WARNING
-                    );
-                } else {
-                    $this->old_name = $this->name;
-                    $this->name     = $value;
-                }
-                break;
-            default:
-                Analog::log(
-                    'Unable to set property ' . $name,
-                    Analog::WARNING
-                );
-                break;
-        }
-    }
 
     /**
      * Get system payment types
@@ -309,13 +140,4 @@ class PaymentType
         return isset($this->getSystemTypes()[$this->id]);
     }
 
-    /**
-     * Simple text representation
-     *
-     * @return string
-     */
-    public function __toString(): string
-    {
-        return $this->getName();
-    }
 }
