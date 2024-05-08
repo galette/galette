95bbbc2edec172062b5daeec2344dd68d8861b42
Johan Cwiklinski <johan@x-tnd.be>
2024-05-08 08:27:30 +0200

    Bump version

--- galette/includes/sys_config/versions.inc.php
+++ galette/includes/sys_config/versions.inc.php
@@ -31,6 +31,6 @@ define('GALETTE_MYSQL_MIN', '5.7');
 define('GALETTE_MARIADB_MIN', '10.4');
 define('GALETTE_PGSQL_MIN', '11')  ;
 define('GALETTE_NIGHTLY', false);
-define('GALETTE_VERSION', 'v1.1.0-dev');
+define('GALETTE_VERSION', 'v1.1.0-rc1');
 define('GALETTE_COMPAT_VERSION', '1.1.0');
 define('GALETTE_DB_VERSION', '1.100');
