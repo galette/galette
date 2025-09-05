4af2630c926e60a5a3a47e82b94bfb69d7127d1a
Johan Cwiklinski <johan@x-tnd.be>
2025-09-05 06:51:24 +0200

    Add VCard QR code

--- galette/lib/Galette/Entity/Adherent.php
+++ galette/lib/Galette/Entity/Adherent.php
@@ -2437,6 +2437,12 @@ class Adherent implements AccessManagementInterface
             return $qrcodes;
         }
 
+        $qrcodes['vcard'] = new QrCode(
+            data: $this->getVCard()->serialize(),
+            label: $this->sname,
+            url: $routeparser->urlFor('memberVCard', ['id' => $this->id])
+        );
+
         if (!empty($this->getEmail())) {
             $qrcodes['email'] = new QrCode(
                 data: 'mailto:' . $this->getEmail(),
