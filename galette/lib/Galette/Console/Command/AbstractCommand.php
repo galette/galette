5c50ec6bf6e55ed8f779c73295872768b9150acc
Johan Cwiklinski <johan@x-tnd.be>
2026-05-03 13:46:47 +0200

    Add license headers check & fix script

--- galette/lib/Galette/Console/Command/AbstractCommand.php
+++ galette/lib/Galette/Console/Command/AbstractCommand.php
@@ -23,7 +23,11 @@ declare(strict_types=1);
 
 namespace Galette\Console\Command;
 
+use Override;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
  * Console abstract command
@@ -32,6 +36,28 @@ use Symfony\Component\Console\Command\Command;
  */
 abstract class AbstractCommand extends Command
 {
+    protected InputInterface $input;
+    protected OutputInterface $output;
+    protected SymfonyStyle $io;
+
+    /**
+     * Initializes the command after the input has been bound and before the input
+     * is validated.
+     *
+     * This is mainly useful when a lot of commands extends one main command
+     * where some things need to be initialized based on the input arguments and options.
+     *
+     * @see InputInterface::bind()
+     * @see InputInterface::validate()
+     */
+    #[Override]
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
     /**
      * Default constructor
      *
