616e16c4bd34f504c894078699a1ab959b630251
Johan Cwiklinski <johan@x-tnd.be>
2026-05-03 16:03:43 +0200

    Use SPDX License

--- tests/pgsql_06.sql
+++ tests/pgsql_06.sql
@@ -1,20 +1,7 @@
 --
--- Copyright © 2003-2026 The Galette Team
---
 -- This file is part of Galette (https://galette.eu).
---
--- Galette is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- Galette is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---  GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with Galette. If not, see <http://www.gnu.org/licenses/>.
+-- SPDX-FileCopyrightText: Copyright © 2003-2026 The Galette Team
+-- SPDX-License-Identifier: GPL-3.0-or-later
 --
 
 DROP SEQUENCE adherents_id_seq;
