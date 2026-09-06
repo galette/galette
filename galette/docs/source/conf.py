b85c35977f76a37a64c06ecc5dfc410019f08027
Johan Cwiklinski <johan@x-tnd.be>
2026-09-06 10:55:10 +0200

    Update apidoc shown version

--- galette/docs/source/conf.py
+++ galette/docs/source/conf.py
@@ -16,7 +16,7 @@ copyright = '2003-2026, The Galette Team'
 author = 'Johan Cwiklinski'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.2'
+release = '1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
